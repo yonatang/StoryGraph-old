acf9d92999a20fceb384ae143fcc2129577e6bfd
yonatang <yonatang@tabtale.com>
2015-04-03 22:50:15 +0300

    better dependency line connection

--- client/app/components/storyCanvas/dependencyLine.js
+++ client/app/components/storyCanvas/dependencyLine.js
@@ -1,31 +1,97 @@
 (function (angular, window, undefined) {
     'use strict';
 
+    var eventWidth = 90,
+        eventHeight = 60;
+
+    function getConnectionDirection(e1, e2) {
+        var xDist = e2.x - e1.x,
+            yDist = e2.y - e1.y;
+
+        if (xDist === 0 && yDist === 0) {
+            return 'above';
+        }
+        var ratio = Math.abs(xDist / yDist);
+        if (ratio < 1) {
+            //x is smaller than y - higher/lower
+            return yDist > 0 ? 'below' : 'above';
+        }
+        return xDist > 0 ? 'left' : 'right';
+    }
+
+    function typeOffset(type) {
+        switch (type) {
+            case 'when':
+                return 0;
+            case 'where':
+                return 1;
+            case 'what':
+                return -1;
+            case 'who':
+                return 2;
+        }
+        return 0;
+    }
+
+    function getConnectionCords(e1, e2, type) {
+        var typeFactor = 4;
+        var x = e1.x, y = e1.y;
+        switch (getConnectionDirection(e1, e2)) {
+            //e2 is above
+            case 'above':
+                x += eventWidth / 2 + typeOffset(type) * typeFactor;
+                break;
+            //e2 is below
+            case 'below':
+                x += eventWidth / 2 + typeOffset(type) * typeFactor;
+                y += eventHeight;
+                break;
+            //e2 is from the right
+            case 'right':
+                y += eventHeight / 2 + typeOffset(type) * typeFactor;
+                break;
+            case 'left':
+                x += eventWidth;
+                y += eventHeight / 2 + typeOffset(type) * typeFactor;
+                break;
+        }
+        return {x: x, y: y};
+    }
+
     angular.module('sg.storyCanvas')
         .directive('sgEventDependency', ['storyGraphService',
-            function(storyGraphService) {
+            function (storyGraphService) {
                 return {
                     replace: true,
                     scope: {
                         dep: '=sgDependency'
                     },
                     template: '<line ' +
-                        'ng-attr-x1="{{getE1().x}}" ng-attr-y1="{{getE1().y}}" ' +
-                        'ng-attr-x2="{{getE2().x}}" ng-attr-y2="{{getE2().y}}" ' +
-                        'class="dep-class" ' +
-                        'ng-class="cssStyle" />',
-                    link: function(scope){
-                        var dependency = scope.dep;
-                        scope.getE1 = function(){
-                            return storyGraphService.getEventById(dependency.fromEventId);
+                    'ng-attr-x1="{{getE1().x}}" ng-attr-y1="{{getE1().y}}" ' +
+                    'ng-attr-x2="{{getE2().x}}" ng-attr-y2="{{getE2().y}}" ' +
+                    'class="dep-class" ' +
+                    'ng-class="cssStyle" />',
+                    link: function (scope) {
+                        var dependency = scope.dep,
+                            e1, e2;
+
+                        scope.getE1 = function () {
+                            return getConnectionCords(e1, e2, dependency.type);
                         };
-                        scope.getE2 = function(){
-                            return storyGraphService.getEventById(dependency.toEventId);
+                        scope.getE2 = function () {
+                            return getConnectionCords(e2, e1, dependency.type);
                         };
-                        scope.$watch('dep.type', function(type){
-                            scope.cssStyle = 'dep-class-'+type;
+                        scope.$watch('dep.type', function (type) {
+                            scope.cssStyle = 'dep-class-' + type;
                         });
+                        scope.$watch('dep.fromEventId', function (e1Id) {
+                            e1 = storyGraphService.getEventById(e1Id);
+                        });
+                        scope.$watch('dep.toEventId', function (e2Id) {
+                            e2 = storyGraphService.getEventById(e2Id);
+                        });
+
                     }
                 };
             }]);
-})(angular,window);
+})(angular, window);
